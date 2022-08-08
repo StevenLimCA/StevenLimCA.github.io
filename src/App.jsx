43813e45090f5727fe236bf8207d22735e011a64
Steven Lim <steven-lim-ca>
2022-08-07 18:55:16 -0700

    added animations

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import MainContent from "./Components/MainContent/MainContent";
 import Footer from "./Components/Footer/Footer";
 import Hero from "./Components/Hero/Hero";
 import ProjectPage from "./Components/Pages/ProjectPage/ProjectPage";
+import Carousel from "./Components/Carousel/Carousel";
 
 function App() {
   return (
--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,24 +1,20 @@
 import React from "react";
 import "./Hero.scss";
 import TypeAnimation from "react-type-animation";
+
 export default function Hero() {
   return (
     <div className="hero">
-      <div className="hero__blur">
-        <div className="hero__quote"></div>
-        <div className="hero__wrap">
-          <p className="hero__text"></p>
-        </div>
-      </div>
-      <div className="hero__shadow">
-        {" "}
-        <h1 className="hero__title">
-          <TypeAnimation
-            cursor={true}
-            sequence={["hi! I am Steven", 4000, "I am a Web Developer!", 3000]}
-          />{" "}
-        </h1>
-      </div>
+      {" "}
+      <div className="cloud"></div>
+      <div className="sun"></div>
+      {/* <div className="hero__blur"></div> */}
+      <h1 className="hero__title">
+        <TypeAnimation
+          cursor={true}
+          sequence={["Hi! I am Steven", 4000, "I am a Web Developer!", 3000]}
+        />
+      </h1>
     </div>
   );
 }
