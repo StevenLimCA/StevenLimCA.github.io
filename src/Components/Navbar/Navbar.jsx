91e29b8eeb930fd0c2d12e695ac7e71e0fe57d9e
Steven Lim <steven-lim-ca>
2022-09-20 18:59:07 -0700

    added link to hero

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,19 +6,27 @@ export default function Navbar() {
       <ul className="navbar__list">
         <li className="navbar__item">
           <div className="navbar__icon--home"></div>
-          <a href="/">Home</a>
+          <a href="/" className="navbar__link">
+            Home
+          </a>
         </li>
         <li className="navbar__item">
           <div className="navbar__icon--about"></div>
-          <a href="#about">About Me</a>
+          <a href="#about" className="navbar__link">
+            About Me
+          </a>
         </li>
         <li className="navbar__item">
           <div className="navbar__icon--projects"></div>
-          <a href="#projects"> Projects</a>
+          <a href="#projects" className="navbar__link">
+            Projects
+          </a>
         </li>
         <li className="navbar__item">
           <div className="navbar__icon--skills"></div>
-          <a href="#skills">Skills</a>
+          <a href="#skills" className="navbar__link">
+            Skills
+          </a>
         </li>
       </ul>
     </nav>
