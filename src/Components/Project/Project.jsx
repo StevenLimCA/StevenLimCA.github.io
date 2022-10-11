b9c53fa155847bcf1598be449f431c70b1a0dbb8
Steven Lim <steven-lim-ca>
2022-10-10 17:05:24 -0700

    added tech stacks styling

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./Project.scss";
-
+import "../TechBadge/TechBadge";
+import TechBadge from "../TechBadge/TechBadge";
+import uuid from "react-uuid";
 export default function Project({ projectObj }) {
   const clickHandler = (e) => {
     e.preventDefault();
     window.open(`${projectObj.url}`, "_blank");
   };
+  console.log(projectObj.backTech);
   return (
     <div className="projects">
       <div className="projects__wrap">
@@ -19,14 +22,18 @@ export default function Project({ projectObj }) {
           <h2 className="projects__title">{projectObj.name}</h2>
           <p className="projects__desc">{projectObj.desc}</p>
           <p className="projects__obj">{projectObj.obj1}</p>
-          <h4 className="projects__subtitle">Front-End Tech</h4>
+          <h4 className="projects__subtitle">Front-End Tech Stacks</h4>
           <p className="projects__tech">
-            {projectObj.frontTech.map((el) => " " + el + " ")}
+            {projectObj.frontTech.map((el) => (
+              <TechBadge key={uuid} tech={el} />
+            ))}
           </p>
-          <h4 className="projects__subtitle">Back-End Tech</h4>
+          <h4 className="projects__subtitle">Back-End Tech Stacks</h4>
           <p className="projects__tech">
-            {projectObj.backTech.map((el) => el + " ")}
-          </p>{" "}
+            {projectObj.backTech.map((el) => (
+              <TechBadge key={uuid} tech={el} />
+            ))}
+          </p>
           <button className="button shrink" onClick={clickHandler}>
             View Now!
           </button>
---
+++ src/Components/TechBadge/TechBadge.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./TechBadge.scss";
+
+export default function TechBadge({ tech }) {
+  return <div className="tech-badge">{tech}</div>;
+}
